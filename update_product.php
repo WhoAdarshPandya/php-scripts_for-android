01b0a190ac8dc0b6c93d9dd075eed995e3823360
WhoAdarshPandya <adarshpandya0@gmail.com>
2020-05-05 15:03:42 +0530

    inital2

---
+++ update_product.php
@@ -0,0 +1,30 @@
+<?php
+    $response = array();
+    define('DB_USER', "root"); // db user
+    define('DB_PASSWORD', ""); // db password (mention your db password here)
+    define('DB_DATABASE', "android_php"); // database name
+    define('DB_SERVER', "localhost:1008");
+
+    $db = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE) or die(mysql_error());
+    if(isset($_GET['name']) && isset($_GET['price']) && isset($_GET['id']))
+    {
+        $name = $_GET['name'];
+        $price = $_GET['price'];
+        $id = $_GET['id'];
+        $result =mysqli_query($db,"UPDATE products SET product_name = '$name' , product_price = '$price' where product_id = '$id'");
+        if($result){
+            $response["success"] = 1;
+            $response["message"] = "User successfully updateed.";
+            echo json_encode($response);
+        }else{
+            $response["success"] = 0;
+            $response["message"] = "failed to update";
+            echo json_encode($response);
+        }
+    }
+    else{
+        $response["success"] = 0;
+        $response["message"] = "missing get/post params";
+        echo json_encode($response);
+    }
+?>
